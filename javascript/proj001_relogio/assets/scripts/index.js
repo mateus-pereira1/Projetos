b7f6f963855a6d21358e462ff69c19d519c3da32
mateus-pereira1 <147281084+mateus-pereira1@users.noreply.github.com>
2026-04-05 17:19:10 -0300

    Atualização de projeto.
    Realizada fatoração em partes do código:
    
    -Código separado por funções.
    -Adicionado formatação dinâmica para formato 24 para 12 horas.
    -Adicionada função de saudação.
    -Adicionada função que mostra data por extenso.
    -Adicionado estilização para centralizar as divis.

--- javascript/proj001_relogio/assets/scripts/index.js
+++ javascript/proj001_relogio/assets/scripts/index.js
@@ -1,17 +1,66 @@
 //Variáveis criadas a partir do HTML 
 let display = window.document.getElementById("display");
+let saudacao = window.document.getElementById("saudacao");
+let data = window.document.getElementById("data");
 
-
-
-
-
+//Variáveis para os dias da semana e os meses
+let diasSemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+let mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 //Função Main
 setInterval(() => {
-    //Utilização do Local Date
+    //Utilização do Local Date. Mostra a data e hora na tela
     const date = new Date();
     let hora = date.getHours();
-    let minutos = date.getMinutes();
-    let segundos = String(date.getSeconds()).padStart(2,'0');
-    display.innerText = `${hora}:${minutos}:${segundos}`
-}, 100);
+    display.innerText = `${mostraHoras()}`;
+
+    //Define a saudação de acordo com o horário
+    mostraSaudacao(hora);
+
+    //ostra o dia, mẽs e ano na tela
+    mostraData(date);
+    
+}, 1000);
+
+
+//Formata as horas
+function formataHora(hour12){
+    return new Intl.DateTimeFormat('pt-br',{
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: hour12
+    });
+}
+
+//Função que usa as horas formatadas
+function mostraHoras(){
+    let formatar = true;
+    const data = new Date();
+    
+    return formataHora(formatar).format(data);
+}
+
+//Função que mostra a data por extenso
+function mostraData(date){
+
+    let diaSemana = diasSemana[date.getDay()]
+    let diaAtual = date.getDate();
+    let mesAtual = mes[date.getMonth()];
+    let anoAtual = date.getFullYear();
+
+
+    return data.innerText = `${diaSemana}, ${diaAtual} de ${mesAtual} de ${anoAtual}`;
+}
+
+//Função que realiza a saudação
+
+function mostraSaudacao(hora){
+    if(hora >= 6 && hora < 12){
+        return saudacao.innerText = `Bom dia!`;
+    } else if(hora >= 12 && hora < 18){
+        return saudacao.innerText = `Boa tarde!`;
+    } else{
+        return saudacao.innerText = `Boa noite!`;
+    }
+}
